d7d6b0f53209fddb9df5640e7bb44a770f5bfe12
AYACHI <zinedine.ayachi@outlook.com>
2023-12-07 13:12:05 +0100

    Etapes 5

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('Acceuil');
+});
+
+Route::get('/cours', function () {
+    return view('cours');
+});
+
+Route::get('/tarifs', function () {
+    return view('tarifs');
+});
+
+Route::get('/tarifs', function () {
+    return view('tarifs');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/mentions-legales', function () {
+    return view('mentions-legales');
 });
